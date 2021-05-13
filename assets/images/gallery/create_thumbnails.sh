71ff1fd24ebe9bcfb345e71334d18b43623d202c
Michael Devery <michaeldevery@gmail.com>
2021-05-13 16:45:00 +0100

    Updating script to avoid long thumbnails

--- assets/images/gallery/create_thumbnails.sh
+++ assets/images/gallery/create_thumbnails.sh
@@ -4,5 +4,5 @@ FILES=$(find . -maxdepth 1 -type f \( -name "*.jpg" -or -name "*.jpeg" -or -name
 for i in $FILES
 do
 echo "Processing image $i ..."
-/usr/bin/convert -thumbnail 400 $i ./thumbnails/$i
+/usr/bin/convert -thumbnail 400x400^ -gravity center -extent 400x400 $i ./thumbnails/$i
 done
